eb250937fd01a5c95b92298da41ed279c8529c8e
cananyavuz <yavuzcanan15@gmail.com>
2023-04-28 13:24:49 +0300

    Gesture Recognizer used.

--- gestureRecognizer/ViewController.swift
+++ gestureRecognizer/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeImg))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
-
+    
+    @objc func changeImg(){
+        
+        if imageView.image == UIImage(named: "ist"){
+            cityLabel.text = "Rize"
+            imageView.image = UIImage(named: "rize")
+        }else{
+            cityLabel.text = "Istanbul"
+            imageView.image = UIImage(named: "ist")
+        }
+        
+    }
 
 }
 
